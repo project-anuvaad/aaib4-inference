c6caee9cd1778c5053d407dd4cc0b96bb3769b77
Sujit27 <sujit271290@gmail.com>
2022-02-15 15:17:35 +0530

    Upgraded the flask server from development to WSGI

--- src/app.py
+++ src/app.py
@@ -27,5 +27,8 @@ for blueprint in vars(routes).values():
         server.register_blueprint(blueprint, url_prefix=config.API_URL_PREFIX)
 
 if __name__ == "__main__":
+    from waitress import serve
+
     log_info('starting server at {} at port {}'.format(config.HOST, config.PORT), MODULE_CONTEXT)
-    server.run(host=config.HOST, port=config.PORT, debug=config.DEBUG, threaded=True)
+    # server.run(host=config.HOST, port=config.PORT, debug=config.DEBUG, threaded=True)
+    serve(server, host=config.HOST, port=config.PORT)

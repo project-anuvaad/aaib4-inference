e7286b1c51e29f571960c600d68f97bdc7786909
Vishal Mahuli <vishal.mahuli@tarento.com>
2022-02-22 14:52:10 +0530

    refactoring in cron req.txt

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask.blueprints import Blueprint
 from flask_cors import CORS
 from anuvaad_auditor.loghandler import log_info
@@ -8,8 +8,7 @@ from utilities import MODULE_CONTEXT
 import threading
 from kafka_wrapper import KafkaTranslate
 
-server = Flask(__name__)
-
+nmt_app = Flask(__name__)
 
 def kafka_function():
     log_info('starting kafka from nmt-server on thread-1', MODULE_CONTEXT)
@@ -21,12 +20,12 @@ if config.bootstrap_server_boolean:
     t1.start()
 
 if config.ENABLE_CORS:
-    cors = CORS(server, resources={r"/api/*": {"origins": "*"}})
+    cors = CORS(nmt_app, resources={r"/api/*": {"origins": "*"}})
 
 for blueprint in vars(routes).values():
     if isinstance(blueprint, Blueprint):
-        server.register_blueprint(blueprint, url_prefix=config.API_URL_PREFIX)
+        nmt_app.register_blueprint(blueprint, url_prefix=config.API_URL_PREFIX)
 
 if __name__ == "__main__":
     log_info('starting server at {} at port {}'.format(config.HOST, config.PORT), MODULE_CONTEXT)
-    server.run(host=config.HOST, port=config.PORT, debug=config.DEBUG, threaded=True)
+    nmt_app.run(host=config.HOST, port=config.PORT, debug=config.DEBUG, threaded=True)

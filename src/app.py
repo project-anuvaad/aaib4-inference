43427819877b6f75b919d62c5ce49df47d50abe8
Navneet3440 <navneetmec3440@gmail.com>
2022-02-28 12:36:24 +0530

    First patched-up fix to implement multiprocessing

--- src/app.py
+++ src/app.py
@@ -1,13 +1,16 @@
+from audioop import mul
+from subprocess import call
 from flask import Flask, jsonify, request
 from flask.blueprints import Blueprint
 from flask_cors import CORS
 from anuvaad_auditor.loghandler import log_info
 import routes
 import config
-from cron_job import NMTcronjob, TranslationScheduler
+from cron_job import NMTcronjob, TranslationScheduler, NMTScheduleProcess
 from utilities import MODULE_CONTEXT
 import threading
 from kafka_wrapper import KafkaTranslate
+import multiprocessing
 
 nmt_app = Flask(__name__)
 
@@ -27,12 +30,22 @@ for blueprint in vars(routes).values():
     if isinstance(blueprint, Blueprint):
         nmt_app.register_blueprint(blueprint, url_prefix=config.API_URL_PREFIX)
 
+def call_nmt_translation_service():
+    log_info(f"Starting the Translation service as a subprocess", MODULE_CONTEXT)
+    NMTScheduleProcess()
+    
 if __name__ == "__main__":
     log_info('starting server at {} at port {}'.format(config.HOST, config.PORT), MODULE_CONTEXT)
+    '''
     translation_scheduler = TranslationScheduler()
     translation_scheduler.schedule()
+    '''
+
     '''
     wfm_jm_thread = NMTcronjob(threading.Event())
     wfm_jm_thread.start()
     '''
+    p1 = multiprocessing.Process(target=call_nmt_translation_service, name='Translation Service')
+    p1.start()
     nmt_app.run(host=config.HOST, port=config.PORT, debug=config.DEBUG, threaded=True)
+    p1.join()

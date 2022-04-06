e25ec4e3a9bc139bce7850782a02f043fc2f9e9f
Navneet3440 <navneetmec3440@gmail.com>
2022-04-06 11:52:23 +0530

    Gunicorn implementation

--- nmt_async_proxy/start.sh
+++ nmt_async_proxy/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 app.py
-#gunicorn -w 4 --threads 4 -b 0.0.0.0:8003 "app:create_app_with_gunicorn()"
+# python3 app.py
+gunicorn -w 4 --threads 25 -b 0.0.0.0:8003 "app:create_app_with_gunicorn()"

5b4559ded5455913e56ff6fa91ff15cb24f8b275
Navneet3440 <navneetmec3440@gmail.com>
2022-04-06 18:29:37 +0530

    Gunicor_10worker_100thread

--- nmt_async_proxy/start.sh
+++ nmt_async_proxy/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # python3 app.py
-gunicorn -w 100 --threads 10 -b 0.0.0.0:5001 "app:create_app_with_gunicorn()"
+gunicorn -w 10 --threads 100 -b 0.0.0.0:5001 "app:create_app_with_gunicorn()"

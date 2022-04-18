60a03ffba36cc7b77306619799793b37a188b164
Navneet3440 <navneetmec3440@gmail.com>
2022-04-18 21:48:43 +0530

    gunicorn disabled

--- nmt_async_proxy/start.sh
+++ nmt_async_proxy/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# python3 app.py
+python3 app.py
 # python3 fastapi_app.py
-gunicorn -w 10 --threads 100 -b 0.0.0.0:5001 "app:create_app_with_gunicorn()"
+# gunicorn -w 10 --threads 100 -b 0.0.0.0:5001 "app:create_app_with_gunicorn()"
 # gunicorn -w 2 --worker-class uvicorn.workers.UvicornWorker -b 0.0.0.0:5001 fastapi_app:app

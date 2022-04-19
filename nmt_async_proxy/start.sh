1e76011f826a6346bd401b509ee8931744354a27
Navneet3440 <navneetmec3440@gmail.com>
2022-04-19 10:56:18 +0530

    gunicorn 2w 250 thread

--- nmt_async_proxy/start.sh
+++ nmt_async_proxy/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # python3 app.py
 # python3 fastapi_app.py
-gunicorn -w 2 --threads 1000 -b 0.0.0.0:5001 "app:create_app_with_gunicorn()"
+gunicorn -w 2 --threads 250 -b 0.0.0.0:5001 "app:create_app_with_gunicorn()"
 # gunicorn -w 2 --worker-class uvicorn.workers.UvicornWorker -b 0.0.0.0:5001 fastapi_app:app

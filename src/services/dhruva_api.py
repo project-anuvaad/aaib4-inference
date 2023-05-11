e2032f58009fae39e09836b0d30914c2ca6aa8d3
AmitTarento <amit.paras@tarento.com>
2023-05-11 13:11:58 +0530

    service-id-added

--- src/services/dhruva_api.py
+++ src/services/dhruva_api.py
@@ -4,18 +4,26 @@ import json as js
 from anuvaad_auditor.loghandler import log_info, log_exception
 from utilities import MODULE_CONTEXT
 
+
 dhurva_url = os.environ.get('DHURVA_URL')
 	
 def dhruva_api_call(src_list, source_language_code, target_language_code):
 
 	access_token = os.environ.get('DHRUVA_ACCESS_TOKEN')
-	
+	if source_language_code == "en":
+		serviceid = os.environ.get('AAI4B_SERVICE_ID_EN_IN')
+	elif target_language_code == "en":
+		serviceid = os.environ.get('AAI4B_SERVICE_ID_IN_EN')
+	elif source_language_code != "en" and target_language_code != "en":
+		serviceid = os.environ.get('AAI4B_SERVICE_ID_IN_IN')
+	else:
+		serviceid = "ai4bharat/indictrans-fairseq-all-gpu--t4"
 	data_json = {
     		"pipelineTasks": [
         		{
             			"taskType": "translation",
             			"config": {
-            			"serviceId": "ai4bharat/indictrans-fairseq-all-gpu--t4",
+            			"serviceId": serviceid,
                 		"language": {
                     			"sourceLanguage": source_language_code,
                     			"targetLanguage": target_language_code

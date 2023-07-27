0c9e152d024ec6db5267509e954c090123ae4d98
AmitTarento <amit.paras@tarento.com>
2023-07-27 13:10:56 +0530

    timeout-added-dhruva

--- src/services/dhruva_api.py
+++ src/services/dhruva_api.py
@@ -40,13 +40,33 @@ def dhruva_api_call(src_list, source_language_code, target_language_code):
 		data_json["inputData"]["input"].append(local_text)
 	
 	headers={'Content-Type':'application/json', 
-		'Authorization': access_token}	
-	response = requests.post(dhurva_url, headers=headers, json=data_json)
-	log_info("Dhruva has been called with content, request-url: {0}, body: {1}, auth_token: {2}, serviceId: {3}".format(dhurva_url, data_json, access_token, serviceid), MODULE_CONTEXT)
-	log_info("Dhruva returned content {0}-{1} |".format(response.status_code, response.text), MODULE_CONTEXT)
-	return response
+		'Authorization': access_token}
+	out = []
+	try:	
+		response = requests.post(dhurva_url, headers=headers, json=data_json, timeout=60)
+		log_info("Dhruva has been called with content, request-url: {0}, body: {1}, auth_token: {2}, serviceId: {3}".format(dhurva_url, data_json, access_token, serviceid), MODULE_CONTEXT)
+		log_info("Dhruva returned content {0}-{1} |".format(response.status_code, response.text), MODULE_CONTEXT)
+		if response.status_code == 200:
+			log_info("Dhruva API Request has beed called, successful | {}".format(response.status_code), MODULE_CONTEXT)
+			response_dict = js.loads(response.text)
+			print(response_dict["pipelineResponse"][0]["output"])
+			for transl in response_dict["pipelineResponse"][0]["output"]:
+				out.append(transl["target"])
+		else:
+			log_info("Dhruva API Request has beed called, Not success {0}-{1} |".format(response.status_code, response.text), MODULE_CONTEXT)
+			for i in range(len(src_list)):
+				out.append("")
+	except requests.Timeout:
+		log_info("DHRUVA TIME OUT CALL HAPPENED", MODULE_CONTEXT)
+		for i in range(len(src_list)):
+			out.append("")
+	except requests.RequestException as e:
+		log_info("Dhruva API Request has beed called, Not success {0}-{1} |".format(response.status_code, response.text), MODULE_CONTEXT)
+		for i in range(len(src_list)):
+			out.append("")
+	return out
 
-
+"""
 def dhruva_api_request(src_list, source_language_code, target_language_code):
 	response = dhruva_api_call(src_list, source_language_code, target_language_code)
 	out = []
@@ -63,4 +83,4 @@ def dhruva_api_request(src_list, source_language_code, target_language_code):
 			out.append("")
 	return out
 	
-
+"""
